62838ea6f2bda64e6592ad521706a10b2176c695
Mrinal Jha <mrinalkumarjha@ymail.com>
2025-07-31 07:27:16 +0530

    added route to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,35 @@ import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MoviesGrid from './components/MoviesGrid';
+import Watchlist from './components/Watchlist';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header ></Header>
-        <MoviesGrid></MoviesGrid>
+
+        <Router>
+          <nav className="nav">
+            <ul className="nav-list">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/watchlist" className="nav-link">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+            
+            <Routes>
+              <Route path="/" element={<MoviesGrid />}></Route>
+              <Route path="/watchlist" element={<Watchlist />}></Route>
+            </Routes>
+        </Router>
+
       </div>
 
       <Footer></Footer>
---
+++ src/components/Watchlist.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles.css';
+
+export default function Watchlist() {
+  return (
+    <div className="watchlist">
+      <h1 className="title">Your Watchlist</h1>
+      <p className="watchlist-message">No movies in your watchlist yet.</p>
+    </div>
+  );
+}
