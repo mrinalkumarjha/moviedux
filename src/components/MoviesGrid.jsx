ac37cd13d09cee06308681e5dd7a2d24fcca6a1c
Mrinal Jha <mrinalkumarjha@ymail.com>
2025-07-31 06:14:28 +0530

    search and filter logic added in grid

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -5,6 +5,7 @@ import MovieCard from './MovieCard';
 export default function MoviesGrid() {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         fetch("movies.json")
@@ -13,12 +14,34 @@ export default function MoviesGrid() {
             .catch(error => console.error('Error fetching movies:', error));
     }, []);
 
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    const filteredMovies = movies.filter(movie =>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return(
-        <div className="movies-grid">
-            {movies.map(movie => (
+        <div>
+            <div className="search-bar">
+                <input 
+                    type="text" className="search-input"
+                    placeholder="Search for a movie..." 
+                    value={searchTerm} 
+                    onChange={handleSearchChange} 
+                />
+            </div>
+
+            <div className="movies-grid">
+                {filteredMovies.map(movie => (
                 <MovieCard movie={movie} key={movie.id} ></MovieCard>
-            ))}
-            
+                ))}
+            </div>
+
+            <h1>Movies</h1>
+            <p>Found {movies.length} movies</p>
         </div>
+     
     );
 }
